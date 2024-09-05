ebd324f7fad55222b4e3b837449fa9b246fd5862
IzadorMeyer <IzadorMeyer@gmail.com>
2024-09-05 23:59:59 +0000

    saving data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -46,6 +46,20 @@ $(document).ready(function () {
   dataShapes.push(shape);
   // TODO 2: add a new property to all data shapes
 
+
+for(var i = 0; i < dataShapes.length; i++){
+var currentShape = dataShapes[i]
+if(currentShape.color === "red"){
+  currentShape.goodBehavior = "bounce"
+}else if(currentShape.color === "blue"){
+  currentShape.goodBehavior = "blink"
+}else{
+  currentShape.goodBehavior = "spin"
+}
+
+}
+
+
   // TODO 3-a: add a function that handles the static display type
 
   // TODO 4-a: add a function that handles the good display type
