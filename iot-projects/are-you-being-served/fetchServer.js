9ae89461dfd01ebb4d1ed11e81c2121365c91bab
isadormeyer <izadormeyer@gmail.com>
2025-02-12 15:38:04 -0600

    saving pong

--- iot-projects/are-you-being-served/fetchServer.js
+++ iot-projects/are-you-being-served/fetchServer.js
@@ -3,4 +3,18 @@ const http = require("http");
 const fetch = require("fetch");
 const port = 8686;
 
-http.createServer(async function (req, res) {}).listen(port);
+http
+  .createServer(async function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    var fetchResult = fetch("https://izadormeyer.github.io");
+
+    if (fetchResponse.ok === true) {
+      var html = await fetchResponse.text();
+      res.write(html);
+    } else {
+      res.write(fetchResponse.statusText && fetchResponse.status);
+    }
+
+    res.end();
+  })
+  .listen(port);
