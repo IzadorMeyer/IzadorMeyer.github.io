8378b81ce8c4c8305636e87047fff7968161b733
isadormeyer <izadormeyer@gmail.com>
2025-02-17 23:17:53 -0600

    saving served

--- iot-projects/are-you-being-served/fetchServer.js
+++ iot-projects/are-you-being-served/fetchServer.js
@@ -6,13 +6,14 @@ const port = 8686;
 http
   .createServer(async function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    varfetchResponse = fetch("https://izadormeyer.github.io");
+    var fetchResponse = fetch("https://izadormeyer.github.io");
 
     if (fetchResponse.ok === true) {
       var html = await fetchResponse.text();
       res.write(html);
     } else {
-      res.write(fetchResponse.statusText && fetchResponse.status);
+      res.write(fetchResponse.statusText);
+      res.write(fetchResponse.status);
     }
 
     res.end();
