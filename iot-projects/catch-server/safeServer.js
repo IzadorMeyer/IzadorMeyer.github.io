2ac77bb02026de003d3ec4d48d6a0a39979e0d49
isadormeyer <izadormeyer@gmail.com>
2025-03-11 19:10:52 -0500

    saving catch

--- iot-projects/catch-server/safeServer.js
+++ iot-projects/catch-server/safeServer.js
@@ -1,21 +1,22 @@
 const http = require("http")
 const port = 3000
 let serverStatus = undefined
+var body = ""
 const server = http.createServer(function(req, res){
     try{
         if(req.method === "GET"){
             res.writeHead(200, {"content-Type": "text/plain"})
-            res.write(serverStatus)
+            res.write(serverStatus.status)
 
         }else if(req.method === "PUT"){
-            var body = ""
+            
             req.on('data', function(){
                 body += 'data'
             })
 
             req.on('end', function(){
                 serverStatus = {}
-                serverStatus.status = serverStatus
+                serverStatus.status = JSON.parse(body)
                 res.writeHead(200, {"content-Type": "text/plain"})
                 res.write("the server has been updated.")
             })
