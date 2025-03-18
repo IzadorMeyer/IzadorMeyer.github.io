9d11af8323c7fcdb9c2ae3d79009e58ea8539598
isadormeyer <izadormeyer@gmail.com>
2025-03-18 18:58:25 -0500

    saving Interface

--- iot-projects/interface-inyourface/sim-server.js
+++ iot-projects/interface-inyourface/sim-server.js
@@ -6,7 +6,27 @@ const WebSocket = require("ws");
 const port = 8080;
 
 //TODO 1: Variables and generateTemperature function
+var temperature = 72
+var nextChange = 0
 
+function generateTemperature(){
+  let changeDiffrence = Math.random() - 0.5
+
+  nextChange += changeDiffrence
+  temperature += nextChange
+
+  if(temperature < 0){
+    temperature = 0
+    nextChange = 0
+  } else if(tempature > 100){
+    tempature = 100
+    nextChange = 0
+    
+  }
+  console.log(tempature)
+ }
+
+ setInterval(generateTemperature, 1000);
 
 // Configure our HTTP server.
 const server = http.createServer(function (req, res) {
@@ -20,8 +40,7 @@ const server = http.createServer(function (req, res) {
   /* DO NOT EDIT THIS CODE */
 
   //TODO 2: Regular Polling Server
-
-
+ 
 });
 
 //TODO 7: WebSocket Server
--- video-game-course/calculate-distance/bower_components/V6/README.md
+++
@@ -1,3 +0,0 @@
-# V6
-
-A CreateJS wrapper to handle initializing the CreateJS framework for demo purposes.
