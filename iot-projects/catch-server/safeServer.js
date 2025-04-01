d5736c7b79a07cc0b335f0a98edc30004f70ece1
isadormeyer <izadormeyer@gmail.com>
2025-04-01 18:52:27 -0500

    finish catch server

--- iot-projects/catch-server/safeServer.js
+++ iot-projects/catch-server/safeServer.js
@@ -6,7 +6,7 @@ const server = http.createServer(function(req, res){
     try{
         if(req.method === "GET"){
             res.writeHead(200, {"content-Type": "text/plain"})
-            res.write(serverStatus)
+            res.write(serverStatus.message)
 
         }else if(req.method === "PUT"){
             var body = ""
@@ -17,6 +17,7 @@ const server = http.createServer(function(req, res){
             
             req.on('end', function(){
                 serverStatus = {}
+                console.log(JSON.parse(body))
                 serverStatus = JSON.parse(body)
             })
             res.writeHead(200, {"content-Type": "text/plain"})
