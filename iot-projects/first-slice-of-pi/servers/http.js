9cbddf402014de4560030731bf0b23550d6a90b7
Izadormeyer <izadormeyer@gmail.com>
2025-04-29 19:51:01 -0500

    saving first slice of pi

--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -1,5 +1,21 @@
 const express = require('express'),
 	cors = require('cors');
-	
+var sensorRoutes =require('./../routes/sensors')
 
-module.exports = app;
+
+
+var app = express()
+app.use(cors())
+app.use('/pi/sensors', sensorRoutes)
+
+app.get("/192.168.0.178", function (req, res) {
+	res.send("Some response for accessing the root");
+  });
+
+  app.get("/pi", function (req, res) {
+	res.send("Some other response for accessing the root");
+  });
+
+  module.exports = app;
+
+// i have looked through all files
