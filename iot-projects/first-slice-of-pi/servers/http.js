222b3bc1cb4ca2cc603e55e3626398cb205a7842
Izadormeyer <izadormeyer@gmail.com>
2025-05-06 19:04:54 -0500

    finished first slice of pi

--- iot-projects/first-slice-of-pi/servers/http.js
+++ iot-projects/first-slice-of-pi/servers/http.js
@@ -1,14 +1,15 @@
 const express = require('express'),
 	cors = require('cors');
-var sensorRoutes =require('./../routes/sensors')
-
+var sensorRoutes = require('./../routes/sensors')
+var actuatorRoutes = require('./../routes/actuators')
 
 
 var app = express()
 app.use(cors())
 app.use('/pi/sensors', sensorRoutes)
+app.use('/pi/actuators', actuatorRoutes)
 
-app.get("/192.168.0.178", function (req, res) {
+app.get("/", function (req, res) {
 	res.send("Some response for accessing the root");
   });
 
