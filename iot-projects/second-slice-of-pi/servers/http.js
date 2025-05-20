9eabf2376c9204f3cf90dd385a2dd6796759e7af
Izadormeyer <izadormeyer@gmail.com>
2025-05-20 17:24:52 -0500

    saving second slice

--- iot-projects/second-slice-of-pi/servers/http.js
+++ iot-projects/second-slice-of-pi/servers/http.js
@@ -1,27 +1,26 @@
-const express = require('express'),
-	cors = require('cors');
-var sensorRoutes = require('./../routes/sensors')
-var actuatorRoutes = require('./../routes/actuators')
+const express = require("express"),
+  cors = require("cors");
+var sensorRoutes = require("./../routes/sensors");
+var actuatorRoutes = require("./../routes/actuators");
 var converter = require("./../middleware/converter");
 var bodyParser = require("body-parser");
 
-
-var app = express()
+var app = express();
 app.use(bodyParser.json());
-app.use(cors())
-app.use('/pi/sensors', sensorRoutes)
-app.use('/pi/actuators', actuatorRoutes)
+app.use(cors());
+app.use("/pi/sensors", sensorRoutes);
+app.use("/pi/actuators", actuatorRoutes);
 
 app.get("/", function (req, res) {
-	res.send("Some response for accessing the root");
-  });
+  res.send("Some response for accessing the root");
+});
 
-  app.get("/pi", function (req, res) {
-	res.send("Some other response for accessing the root");
-  });
+app.get("/pi", function (req, res) {
+  res.send("Some other response for accessing the root");
+});
 
-  app.use(converter());
+app.use(converter());
 
-  module.exports = app;
+module.exports = app;
 
 // i have looked through all files
